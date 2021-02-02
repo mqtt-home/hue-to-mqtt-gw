64477b772be7d450e918c9b5abc6b1314f8db0f3
Philipp Arndt <2f.mail@gmx.de>
2021-02-02 06:16:50 +0100

    handle null values

--- src/de.rnd7.huemqtt/src/test/java/de/rnd7/huemqtt/hue/LightDeviceTest.java
+++ src/de.rnd7.huemqtt/src/test/java/de/rnd7/huemqtt/hue/LightDeviceTest.java
@@ -1,4 +1,23 @@
-import static org.junit.jupiter.api.Assertions.*;
+package de.rnd7.huemqtt.hue;
+
+import de.rnd7.mqttgateway.Message;
+import io.github.zeroone3010.yahueapi.Light;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
 class LightDeviceTest {
-  
+
+    @Test
+    void test_null_message() {
+        Light light = Mockito.mock(Light.class);
+        new LightDevice(light,"", "")
+            .onMessage(new Message("/set", null));
+    }
+
+    @Test
+    void test_message() {
+        Light light = Mockito.mock(Light.class);
+        new LightDevice(light,"", "")
+            .onMessage(new Message("/set", "{\"state\":\"OFF\",\"brightness\":1,\"color_temp\":366}"));
+    }
 }
