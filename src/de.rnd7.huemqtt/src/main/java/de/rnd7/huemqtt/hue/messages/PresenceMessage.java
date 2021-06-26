f586906ee061e675789ae8de4d6ef5c4b75a594f
Philipp Arndt <2f.mail@gmx.de>
2021-06-26 20:01:35 +0200

    remove presence code

--- src/de.rnd7.huemqtt/src/main/java/de/rnd7/huemqtt/hue/messages/PresenceMessage.java
+++ src/de.rnd7.huemqtt/src/main/java/de/rnd7/huemqtt/hue/messages/PresenceMessage.java
@@ -18,24 +18,16 @@ public class PresenceMessage {
     @SerializedName("presence")
     private boolean presence;
 
-    @SerializedName("presence_code")
-    private int presenceCode;
-
     @SerializedName("last-updated")
     private ZonedDateTime lastUpdated;
 
     public PresenceMessage setPresence(final boolean presence) {
         this.presence = presence;
-        this.presenceCode = presence ? 1 : 0;
         return this;
     }
 
     public boolean isPresence() {
         return this.presence;
-    }
-
-    public int getPresenceCode() {
-        return presenceCode;
     }
 
     public PresenceMessage setLastUpdated(final ZonedDateTime lastUpdated) {
