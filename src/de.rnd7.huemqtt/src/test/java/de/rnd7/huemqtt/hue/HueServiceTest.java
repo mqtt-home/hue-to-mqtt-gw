749b48c0d5f0ce71056ca94da29360c74c80be12
Philipp Arndt <2f.mail@gmx.de>
2021-12-12 13:14:48 +0100

    service clean-up

--- src/de.rnd7.huemqtt/src/test/java/de/rnd7/huemqtt/hue/HueServiceTest.java
+++ src/de.rnd7.huemqtt/src/test/java/de/rnd7/huemqtt/hue/HueServiceTest.java
@@ -2,11 +2,18 @@ package de.rnd7.huemqtt.hue;
 
 import de.rnd7.huemqtt.hue.api.HueAbstraction;
 import features.HueAbstractionStub;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class HueServiceTest {
+
+    @AfterEach
+    public void tearDown(){
+        HueService.shutdown();
+    }
+
     @Test
     void test_start_twice() {
         final HueAbstraction hue = new HueAbstractionStub();
