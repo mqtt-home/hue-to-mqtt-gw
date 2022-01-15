95070375b360384c7e6624b1a95197149a63e638
Philipp Arndt <2f.mail@gmx.de>
2022-01-15 13:37:05 +0100

    init event source

--- hue-to-mqtt/lib/index.test.ts
+++
@@ -1,7 +0,0 @@
-import { add } from "./index"
-
-describe("Calculator", () => {
-    test("add", async () => {
-        expect(add(4, 3)).toBe(7)
-    })
-})
--- hue-to-mqtt/lib/index.ts
+++ hue-to-mqtt/lib/index.ts
@@ -3,6 +3,25 @@ import config from "./config.json"
 import axios from "axios"
 import * as https from "https"
 
-export const add = (a: number, b: number) => a + b
+import cron from "node-cron"
+import { initStateManagerFromHue } from "./state/state-manager"
+import { startSSE } from "./SSEClient"
+
+const triggerFullUpdate = async () => {
+    log.info("Updating devices")
+    await initStateManagerFromHue()
+    log.info("Updating devices done")
+}
+
+triggerFullUpdate().then(() => {
+    startSSE()
+    log.info("Application is now ready.")
+
+    // cron.schedule("*/15 * * * * *", () => {
+    //     log.info("You will see this message every /15 second")
+    // }).start()
+
+    cron.schedule("0 * * * *", triggerFullUpdate).start()
+})
 
 
