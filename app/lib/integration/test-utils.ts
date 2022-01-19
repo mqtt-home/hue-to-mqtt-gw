92c55f741bf7a7c381e905241233f2e488046860
Philipp Arndt <2f.mail@gmx.de>
2022-01-19 17:31:09 +0100

    timeouts

--- app/lib/integration/test-utils.ts
+++ app/lib/integration/test-utils.ts
@@ -4,10 +4,10 @@ export const JEST_DEFAULT_TIMEOUT = 5 * 1000
 export const curlHealthTest = (host: string, port: number) => {
     return {
         test: `curl -f http://${host}:${port} || exit 1`,
-        interval: 5000,
-        timeout: 3000,
+        interval: 5_000,
+        timeout: 30_000,
         retries: 20,
-        startPeriod: 1000
+        startPeriod: 1_000
     }
 }
 
