d779d7975570b7e915d06f3273466cd2e4900b40
Philipp Arndt <2f.mail@gmx.de>
2024-05-11 08:53:14 +0200

    feat: improve resource loading

--- app/lib/app.ts
+++ app/lib/app.ts
@@ -2,24 +2,17 @@ import { connectMqtt } from "./mqtt/mqtt-client"
 import { destroySSE, initSSE } from "./SSEClient"
 import { log } from "./logger"
 import cron from "node-cron"
-import { initStateManagerFromHue } from "./state/state-manager"
-import { loadAllResources } from "./api/v2/hue-api-v2"
+import { initStateManagerFromHue, state } from "./state/state-manager"
 
 export const triggerFullUpdate = async () => {
     log.info("Updating devices")
     await initStateManagerFromHue()
-    log.info("Updating devices done")
+    log.info("Updating devices done", `${state._typedResources.size} resources`)
 }
 
 export const startApp = async () => {
     const mqttCleanUp = await connectMqtt()
     await triggerFullUpdate()
-
-    const level = log.level()
-    if (level === "DEBUG") {
-        const resources = await loadAllResources()
-        log.debug("All resources", JSON.stringify(resources))
-    }
 
     initSSE()
 
