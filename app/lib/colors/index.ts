350b829ee7d49ddfdf24a1add8ce0a7c2c496be9
Philipp Arndt <2f.mail@gmx.de>
2026-04-22 20:02:00 +0200

    ci

--- app/lib/colors/index.ts
+++
@@ -1,7 +0,0 @@
-import { convertToRGB } from "./color-rgb"
-import { convertToXY } from "./color-xy"
-
-export const colors = {
-    toRGB: convertToRGB,
-    toXY: convertToXY
-}
--- app/lib/topic/topic-utils.ts
+++
@@ -1,7 +0,0 @@
-export const cleanTopic = (topic: string) => {
-    return topic.toLowerCase()
-        .replace(/ /g, "-")
-        .replace(/ä/g, "ae")
-        .replace(/ö/g, "oe")
-        .replace(/ü/g, "ue")
-}
